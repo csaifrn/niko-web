262bba20c3ff0fa881c2585c814a6597e23db55e
Leonardo Minora <leonardo.minora@gmail.com>
2022-10-31 12:22:58 -0300

    Adicianado exemplo de uso de componentes

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
-import React from 'react'
-import { Title, Wrapper } from './Index'
+import React from 'react';
+import { Card, Title, Wrapper } from './components';
 
 function App() {
   return (
     <Wrapper>
        <Title>Niko Web</Title>     
+       <Card />
     </Wrapper>
   )
 }

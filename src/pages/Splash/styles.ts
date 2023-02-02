8a4fa104f7b897a276fa6c04ff689e19651db386
Pedro Henrique de Sousa Varela <p.varela@academico.ifrn.edu.br>
2023-02-02 13:44:21 -0300

    scope: splash feito com loading (retirar rota splash se não for usar)

--- src/pages/Splash/styles.ts
+++ src/pages/Splash/styles.ts
@@ -7,18 +7,21 @@ export const backgroundContainer = styled.main`
     display: flex;
     justify-content: center;
     align-items: center;
+    background-color: #0A090E;
 `;
 
 export const itemsContainer = styled.div`
     display: flex;
     flex-direction: column;
+    justify-content: center;
+    align-items: center;
     gap: 1.5rem;
 `;
 
 export const logo = styled.img`
-    width: 15rem;
+    width: 5rem;
 
     ${media.greaterThan("medium")`
-       width: 20rem;
+       width: 10rem;
     `}
 `;

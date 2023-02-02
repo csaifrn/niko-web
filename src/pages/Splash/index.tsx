7cba75c36a705b3d045131ca48f14fb8aa0f21bf
RafaelaMicaela <rafaela.micaela@academico.ifrn.edu.br>
2023-02-02 11:45:35 -0300

    style: update logo

--- src/pages/Splash/index.tsx
+++ src/pages/Splash/index.tsx
@@ -3,9 +3,9 @@ import * as S from "./styles"
 export const Splash =() =>{
     return(
         <S.backgroundContainer>
-            <S.itemsContainer>
-                <S.logo alt="Logo" src="{Assets.Logo}"></S.logo>
-            </S.itemsContainer>
+        <S.itemsContainer>
+            <S.logo alt="Logo" src="{Assets.LogoNiko}"></S.logo>
+        </S.itemsContainer>
         </S.backgroundContainer>
     );
 }

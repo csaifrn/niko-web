8ca49e60d4347a40bdfdfbfceac23160a8e875db
Pedro Henrique de Sousa Varela <p.varela@academico.ifrn.edu.br>
2023-05-12 15:49:46 -0300

    feat: data of user static and search user

--- src/data/UserData.ts
+++ src/data/UserData.ts
@@ -2,7 +2,7 @@ const Users = [
     {
       id: 1,
       name: "Pedro Varela",
-      url: "https://img.freepik.com/free-photo/handsome-cheerful-man-with-happy-smile_176420-18028.jpg",
+      url: "https://th.bing.com/th/id/OIP.lhR5XN7V2Np1BY6B5q5_9gAAAA?pid=ImgDet&w=300&h=404&rs=1",
       lote: "Lote 201",
       fase: "catalogacao"
     },
@@ -10,6 +10,20 @@ const Users = [
       id: 2,
       name: "Luís Gustavo",
       url: "https://img.freepik.com/free-photo/handsome-cheerful-man-with-happy-smile_176420-18028.jpg",
+      lote: "",
+      fase: ""
+    },
+    {
+      id: 3,
+      name: "José Lucas",
+      url: "https://th.bing.com/th/id/OIP.jTpsrJxMrIRi5qsxX8RWRQHaLJ?pid=ImgDet&rs=1",
+      lote: "Lote 220",
+      fase: "upload"
+    },
+    {
+      id: 4,
+      name: "Pedro Lucas",
+      url: "https://th.bing.com/th/id/OIP.bs3eojDjgbugZiVSZD79wwAAAA?pid=ImgDet&w=400&h=600&rs=1",
       lote: "",
       fase: ""
     }
--- src/pages/Coordenador/Operador/index.tsx
+++ src/pages/Coordenador/Operador/index.tsx
@@ -1,21 +1,45 @@
-
+import React, { useState } from "react";
 import OperadorCard from "../../../components/OperadorCard";
 import Users from "../../../data/UserData";
 import * as S from "./styles";
+import diacriticless from "diacriticless";
 
 const Operador = () => {
-    console.log(Users.map((user:any)=> user))
-    return(
-        <>
-            <S.CardsArea>
-                {
-                    Users.map((user: any, index: number) => (
-                        <OperadorCard User={user} key={user.id}/>
-                    ))
-                }
-            </S.CardsArea>
-        </>
-    );
-}
+  const [searchTerm, setSearchTerm] = useState(""); 
+
+
+  const handleSearchChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchTerm(event.target.value); // Atualiza o critério de pesquisa conforme o usuário digita
+  };
+
+  const filteredUsers = Users.filter((user: any) => {
+    const userName = diacriticless(user.name.toLowerCase());
+    const search = diacriticless(searchTerm.toLowerCase());
+    return userName.includes(search);
+  });
+
+  const sortedUsers = filteredUsers.sort((a: any, b: any) => {
+    const nameA = diacriticless(a.name.toLowerCase());
+    const nameB = diacriticless(b.name.toLowerCase());
+    return nameA.localeCompare(nameB);
+  });
+
+  return (
+    <>
+      <S.CardsArea>
+      <input
+          type="text"
+          value={searchTerm}
+          onChange={handleSearchChange}
+          placeholder="Pesquisa"
+          style={{height: "44px", padding: "0 2em", borderRadius: "5px", backgroundColor: "rgb(57, 62, 75)", color: "#838383", border: "none"}}
+        />
+        {sortedUsers.map((user: any, index: number) => (
+          <OperadorCard User={user} key={user.id} />
+        ))}
+      </S.CardsArea>
+    </>
+  );
+};
 
 export default Operador;

28ca99c8ae92c33a00e54e503e4c0ea1fa3b3088
Pedro Henrique de Sousa Varela <p.varela@academico.ifrn.edu.br>
2023-05-12 16:41:00 -0300

    feat: linha incorreta

--- src/components/AtribuirModal/index.tsx
+++ src/components/AtribuirModal/index.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import {CardContainer, DataStrongCard, FigureIconCard, ImgIconCard, NumberCard, TextBoxCard} from "./styles";
 
 interface AtribuirModalProps {
     nameUser: string;

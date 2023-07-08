7da88a0c0dedd1e3ade6d014e356d5fbac0bbea5
NicholasTavares <nicholasbalby@hotmail.com>
2023-07-08 12:14:15 -0300

    feat: react query provider

--- src/main.tsx
+++ src/main.tsx
@@ -3,19 +3,24 @@ import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 import { AuthProvider } from 'react-auth-kit';
 import { ThemeProvider } from 'styled-components';
+import { QueryClient, QueryClientProvider } from 'react-query';
 import theme from './global/theme';
 
+const queryClient = new QueryClient();
+
 createRoot(document.getElementById('root') as HTMLElement).render(
-  <AuthProvider
-    authType={'localstorage'}
-    authName={'_auth'}
-    cookieDomain={window.location.hostname}
-    cookieSecure={false}
-  >
-    <ThemeProvider theme={theme}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </ThemeProvider>
-  </AuthProvider>,
+  <QueryClientProvider client={queryClient}>
+    <AuthProvider
+      authType={'localstorage'}
+      authName={'_auth'}
+      cookieDomain={window.location.hostname}
+      cookieSecure={false}
+    >
+      <ThemeProvider theme={theme}>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </ThemeProvider>
+    </AuthProvider>
+  </QueryClientProvider>,
 );

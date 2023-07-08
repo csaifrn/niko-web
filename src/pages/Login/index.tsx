0e29f973c187b7e53b1c5772f716c46ecf0b580f
NicholasTavares <nicholasbalby@hotmail.com>
2023-07-08 12:24:36 -0300

    refactor: page login

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,84 +1,89 @@
-// import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
-import { useContext, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import axios from 'axios';
 import * as S from './styles';
-import { useSignIn } from 'react-auth-kit';
+import { useMutation } from 'react-query';
+import { signIn } from '../../api/services/authentication/signIn';
+import { useRef, useState } from 'react';
+import { SignInResponse, ApiError } from '../../api/services/authentication/signIn/signIn.interface';
+import { validationLoginSchema } from './validation';
+import { ErrorsForm } from './login.interface';
+import * as Yup from 'yup';
 
 const Login = () => {
-  const [resgisterEmail, setRegisterEmail] = useState('');
-  const [resgisterPassword, setRegisterPassword] = useState('');
-  const signIn = useSignIn();
-
-  const user = { email: resgisterEmail, password: resgisterPassword };
-
-  // const auth = getAuth();
-
+  const emailInputRef = useRef<HTMLInputElement>(null);
+  const passwordInputRef = useRef<HTMLInputElement>(null);
+  const [responseError, setResponseError] = useState('');
+  const [validationFormError, setValidationFormError] = useState<ErrorsForm>({ email: '', password: '' });
   const navigate = useNavigate();
 
-  const handleSignUp = async () => {
-    //   try{
-    //     const user = await signInWithEmailAndPassword(auth, resgisterEmail, resgisterPassword)
-    //     console.log("Created", user)
-    //     if(user != null)
-    //     {
-    //         navigate('/');
-    //     }
-    //   } catch (err) {
-    //     console.log(err)
-    //   }
+  const loginMutation = useMutation(signIn, {
+    onSuccess: (data: SignInResponse) => {
+      localStorage.setItem('token', data.token);
+      navigate('/Projetos');
+    },
+    onError: (error: ApiError) => {
+      setResponseError(error.response?.data.message || 'Um erro inesperado ocorreu.');
+    },
+  });
+
+  const validateForm = async (): Promise<boolean> => {
     try {
-      axios.post('http://localhost:3333/auth/sign-in', user).then((response: any) => {
-        const token = response.data.token;
-        // const role = [123, 231];
-        const user = response.data.user;
-        const role = ['Coordenador'];
+      await validationLoginSchema.validate(
+        {
+          email: emailInputRef.current?.value,
+          password: passwordInputRef.current?.value,
+        },
+        {
+          abortEarly: false,
+        },
+      );
+    } catch (error) {
+      if (error instanceof Yup.ValidationError) {
+        const validationErrors = error.inner.reduce<ErrorsForm>((errors, err) => {
+          errors[err.path as keyof ErrorsForm] = err.message;
+          return errors;
+        }, {});
+        setValidationFormError(validationErrors);
+      }
+      return false;
+    }
+    setValidationFormError({});
+    return true;
+  };
 
-        signIn({
-          token: token,
-          expiresIn: 3600,
-          tokenType: 'Bearer',
-          authState: { id: 1, user, role, email: resgisterEmail },
-        });
+  const onSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
 
-        navigate('/Projetos');
+    const isValid = await validateForm();
+
+    if (isValid) {
+      loginMutation.mutate({
+        email: emailInputRef.current!.value,
+        password: passwordInputRef.current!.value,
       });
-    } catch (err) {
-      console.log(err);
     }
   };
 
   return (
-    <>
-      <S.backgroundContainer>
-        <S.itemsContainer>
-          <S.logo alt="Logo" src="Logo_Niko.svg"></S.logo>
-          <S.formLogin>
-            <S.dataArea>
-              <S.labelLogin>Email</S.labelLogin>
-              <S.inputLogin
-                placeholder="Email"
-                onChange={(event) => {
-                  setRegisterEmail(event.target.value);
-                }}
-              ></S.inputLogin>
-            </S.dataArea>
-            <S.dataArea>
-              <S.labelLogin>Senha</S.labelLogin>
-              <S.inputLogin
-                type="password"
-                placeholder="Senha"
-                onChange={(event) => {
-                  setRegisterPassword(event.target.value);
-                }}
-              ></S.inputLogin>
-              <S.dangerText>Esqueceu a senha?</S.dangerText>
-            </S.dataArea>
-          </S.formLogin>
-          <S.btnLogin onClick={handleSignUp}>Entrar</S.btnLogin>
-        </S.itemsContainer>
-      </S.backgroundContainer>
-    </>
+    <S.Wrapper>
+      <S.FormContainer>
+        <S.Logo alt="Logo do sistema Niko em laranja." src="Logo_Niko.svg"></S.Logo>
+        <S.FormLogin onSubmit={onSubmit}>
+          <S.FieldContainer>
+            <S.LabelField htmlFor="email">Email</S.LabelField>
+            <S.InputText ref={emailInputRef} id="email" type="text" placeholder="maria@email.com" />
+            <S.ErrorMessage>{validationFormError.email}</S.ErrorMessage>
+          </S.FieldContainer>
+          <S.FieldContainer>
+            <S.LabelField htmlFor="password">Senha</S.LabelField>
+            <S.InputText ref={passwordInputRef} id="password" type="password" placeholder="Senha" />
+            <S.ErrorMessage>{validationFormError.password}</S.ErrorMessage>
+          </S.FieldContainer>
+          <S.ForgotPasswordLink to="/">Esqueceu a senha?</S.ForgotPasswordLink>
+          <S.ErrorMessage>{responseError}</S.ErrorMessage>
+          <S.SubmitButton type="submit">Entrar</S.SubmitButton>
+        </S.FormLogin>
+      </S.FormContainer>
+    </S.Wrapper>
   );
 };
 

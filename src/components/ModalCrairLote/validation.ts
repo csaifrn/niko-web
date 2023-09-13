ade2a4de1c4a257c3235ca5a4a97d5a848e66ca3
Pedro Henrique de Sousa Varela <pedrao.varela@gmail.com>
2023-09-13 14:45:21 -0300

    refactor: include min length

--- src/components/ModalCrairLote/validation.ts
+++ src/components/ModalCrairLote/validation.ts
@@ -1,5 +1,7 @@
 import * as Yup from 'yup';
 
 export const validationLoginSchema = Yup.object().shape({
-  settlement_project: Yup.string().required('Preencha com um nome.'),
+  settlement_project: Yup.string()
+    .min(3, 'Projeto de assentamento deve ter ao menos 3 caracteres.')
+    .required('Preencha com um nome.'),
 });

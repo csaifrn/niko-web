90df0fc0cc1e240a4cddb2f23579c78c8d5d04e1
Pedro Henrique de Sousa Varela <p.varela@academico.ifrn.edu.br>
2023-09-15 16:14:51 -0300

    refactor: post batch

--- src/api/services/batches/create-batche/index.ts
+++ src/api/services/batches/create-batche/index.ts
@@ -1,10 +1,15 @@
 import axiosInstance from '../../../axiosInstance';
 import { CreateResponseBatche, CreateBatcheParams } from './create.interface';
 
-export const CreateBatche = async ({ settlement_project }: CreateBatcheParams): Promise<CreateResponseBatche> => {
+export const CreateBatche = async ({
+  settlement_project,
+  priority = false,
+}: CreateBatcheParams): Promise<CreateResponseBatche> => {
   const Batche = await axiosInstance.post<CreateResponseBatche>('/Batches', {
     settlement_project,
+    priority,
   });
 
   return Batche.data;
 };
+

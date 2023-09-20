a38ae8e90ee7615782375443b66c3e72f3505ad5
Pedro Henrique de Sousa Varela <pedrao.varela@gmail.com>
2023-09-20 16:16:31 -0300

    refactor: add category interface

--- src/api/services/batches/get-batche/get.interface.ts
+++ src/api/services/batches/get-batche/get.interface.ts
@@ -8,13 +8,19 @@ export interface GetResponseBatche {
   digital_files_count: number;
   physical_files_count: number;
   priority: number;
-  shelf_number: null | number;
+  shelf_number: any;
   created_at: string;
   updated_at: string;
   created_by: CreatedBy;
+  category: Category;
 }
 
 export interface CreatedBy {
   user_id: string;
+  name: string;
+}
+
+export interface Category {
+  settlement_project_category_id: string;
   name: string;
 }

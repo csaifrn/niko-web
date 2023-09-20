a63c59ead80d2ef17dacd7f19902f4cb876fca88
Pedro Henrique de Sousa Varela <pedrao.varela@gmail.com>
2023-09-20 16:22:38 -0300

    refactor: add category_id validation

--- src/components/ModalCrairLote/validation.ts
+++ src/components/ModalCrairLote/validation.ts
@@ -4,4 +4,9 @@ export const validationLoginSchema = Yup.object().shape({
   settlement_project: Yup.string()
     .min(3, 'Projeto de assentamento deve ter ao menos 3 caracteres.')
     .required('Preencha com um nome.'),
+  settlement_project_category_id: Yup.string().required('Escolha uma categoria.'),
+});
+
+export const validationSearch = Yup.object().shape({
+  name: Yup.string().required('Digite o nome da categoria.').min(3, 'Categoria precisa de no mínimo 3 caracteres'),
 });

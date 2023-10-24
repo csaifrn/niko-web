57d8a5d3bc738e3eca4dd9ff0f1a9d9b098539ec
Pedro Henrique de Sousa Varela <78501819+Pedrohsv1@users.noreply.github.com>
2023-10-24 16:06:34 -0300

    refactor: folder management

--- src/components/Observation-modal-create/validation.ts
+++
@@ -1,7 +0,0 @@
-import * as Yup from 'yup';
-
-export const validationObservation = Yup.object().shape({
-  observation: Yup.string()
-    .required('Digite a observação na caixa acima ^^')
-    .min(3, 'Insira 3 caracteres ou mais, por favor.'),
-});
--- src/components/Observation/validation.observation.ts
+++
@@ -1,5 +0,0 @@
-import * as Yup from 'yup';
-
-export const validationObservationSchema = Yup.object().shape({
-  observation: Yup.string().required('Insira seu email.').min(3, 'Insira 3 caracteres ou mais, por favor.'),
-});

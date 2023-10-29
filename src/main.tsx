425e923657f94bd4e476365211648c611725abee
Nicholas.Moreno <nicholas.balby@getinfo.net.br>
2023-10-28 23:47:47 -0300

    feat: config context

--- src/main.tsx
+++ src/main.tsx
@@ -6,32 +6,35 @@ import { ThemeProvider } from 'styled-components';
 import { QueryClient, QueryClientProvider } from 'react-query';
 import theme from './global/theme';
 import { Toaster } from 'react-hot-toast';
+import { UseSharedState } from './context/SharedContext';
 
 const queryClient = new QueryClient();
 
 createRoot(document.getElementById('root') as HTMLElement).render(
-  <QueryClientProvider client={queryClient}>
-    <AuthProvider
-      authType={'localstorage'}
-      authName={'_auth'}
-      cookieDomain={window.location.hostname}
-      cookieSecure={false}
-    >
-      <ThemeProvider theme={theme}>
-        <Toaster
-          position="top-center"
-          reverseOrder={true}
-          containerStyle={{
-            zIndex: '1000',
-            borderRadius: '5px',
-            color: theme.colors.white,
-            fontFamily: 'Rubik',
-          }}
-        />
-        <BrowserRouter>
-          <App />
-        </BrowserRouter>
-      </ThemeProvider>
-    </AuthProvider>
-  </QueryClientProvider>,
+  <UseSharedState>
+    <QueryClientProvider client={queryClient}>
+      <AuthProvider
+        authType={'localstorage'}
+        authName={'_auth'}
+        cookieDomain={window.location.hostname}
+        cookieSecure={false}
+      >
+        <ThemeProvider theme={theme}>
+          <Toaster
+            position="top-center"
+            reverseOrder={true}
+            containerStyle={{
+              zIndex: '1000',
+              borderRadius: '5px',
+              color: theme.colors.white,
+              fontFamily: 'Rubik',
+            }}
+          />
+          <BrowserRouter>
+            <App />
+          </BrowserRouter>
+        </ThemeProvider>
+      </AuthProvider>
+    </QueryClientProvider>
+  </UseSharedState>,
 );

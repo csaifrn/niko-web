74e81f0b6765bf485ca241008c15f78ebe4caa23
Pedro Henrique de Sousa Varela <78501819+Pedrohsv1@users.noreply.github.com>
2023-10-31 14:51:32 -0300

    refactor: board changer name

--- src/components/BoardChanger/styles.ts
+++ src/components/BoardChanger/styles.ts
@@ -3,6 +3,17 @@ import styled from 'styled-components';
 export const ChangeContainer = styled.nav`
   border-radius: 20px;
   justify-content: center;
+  align-items: center;
+  padding: 0 2em;
+  display: flex;
+  flex-direction: column;
+  gap: 1em;
+  margin-bottom: 1em;
+`;
+
+export const Change = styled.nav`
+  border-radius: 20px;
+  justify-content: center;
   padding: 0 2em;
   display: flex;
   gap: 1em;
@@ -27,4 +38,8 @@ export const Barra = styled.div<BarraL>`
   width: 100%;
   height: 0.5em;
   border-radius: 10px;
+`;
+
+export const Title = styled.h1`
+  color: ${({ theme }) => theme.colors['white']};
 `;

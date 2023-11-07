8f7f895c1884988fe31e9669e7eebbdca52e84c7
Pedro Henrique de Sousa Varela <78501819+Pedrohsv1@users.noreply.github.com>
2023-11-07 17:13:40 -0300

    refactor: service post assigners

--- src/api/services/batches/assigners/post-assigners/index.ts
+++ src/api/services/batches/assigners/post-assigners/index.ts
@@ -1,12 +1,11 @@
 import axiosInstance from '../../../../axiosInstance';
-import { GetResponseBatche } from '../../get-batche/get.interface';
-import { PostAssignersParams } from './post.interface';
+import { PostAssignersParams, PostResponseAssigners } from './post.interface';
 
 export const PostAssigners = async ({
   batch_id,
   assignment_users_ids,
-}: PostAssignersParams): Promise<GetResponseBatche> => {
-  const Assigners = await axiosInstance.post<GetResponseBatche>(`/batches/${batch_id}/assignment`, {
+}: PostAssignersParams): Promise<PostResponseAssigners> => {
+  const Assigners = await axiosInstance.post<PostResponseAssigners>(`/batches/${batch_id}/assignment`, {
     assignment_users_ids,
   });
 
--- src/api/services/batches/assigners/post-assigners/post.interface.ts
+++ src/api/services/batches/assigners/post-assigners/post.interface.ts
@@ -2,3 +2,45 @@ export interface PostAssignersParams {
   batch_id: string;
   assignment_users_ids: string[];
 }
+
+export interface PostResponseAssigners {
+  id: string;
+  title: string;
+  digital_files_count: number;
+  physical_files_count: number;
+  priority: boolean;
+  shelf_number: any;
+  created_at: string;
+  updated_at: string;
+  created_by: CreatedBy;
+  category: Category;
+  assignedUsers: AssignedUser[];
+  observations: Observation[];
+}
+
+export interface CreatedBy {
+  user_id: string;
+  name: string;
+}
+
+export interface Category {
+  settlement_project_category_id: string;
+  name: string;
+}
+
+export interface AssignedUser {
+  id: string;
+  name: string;
+}
+
+export interface Observation {
+  id: string;
+  observation: string;
+  created_by: CreatedBy2;
+  created_at: string;
+}
+
+export interface CreatedBy2 {
+  user_id: string;
+  name: string;
+}

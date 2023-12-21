98f9c5709171925d9cbe8be9f3c4a374590e41a0
Pedro Henrique de Sousa Varela <pedrao.varela@gmail.com>
2023-12-21 15:10:31 -0300

    refactor: atributes names

--- src/api/services/batches/query-batches/get.interface.ts
+++ src/api/services/batches/query-batches/get.interface.ts
@@ -1,8 +1,9 @@
-import { Batche } from './../get-batche/get.interface';
+import { AssignedUser, Batche } from './../get-batche/get.interface';
 export interface GetBatcheParams {
   status: number;
 }
 
 export interface GetResponseBatche extends Batche {
-  pending_batch_observations: {};
+  pending_batch_observations: number;
+  assignedUsers: AssignedUser[];
 }

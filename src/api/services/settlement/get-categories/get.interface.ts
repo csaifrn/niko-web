e8999bea1098fddab1e5fbb2be6088a8e506e067
NicholasTavares <nicholasbalby@hotmail.com>
2024-01-21 12:14:26 -0300

    refactor: att categoty interface

--- src/api/services/settlement/get-categories/get.interface.ts
+++ src/api/services/settlement/get-categories/get.interface.ts
@@ -1,4 +1,20 @@
 export interface Category {
   id: string;
   name: string;
+  user_id: string;
+  created_at: Date;
+  updated_at: Date;
+  batch_count: '4';
+  preparation: Phase;
+  cataloguing: Phase;
+  digitization_scanning: Phase;
+  upload: Phase;
+  archiving: Phase;
+}
+
+interface Phase {
+  batch_count: string;
+  available: string;
+  in_progress: string;
+  done: string;
 }
--- src/api/services/settlement/get-categories/index.ts
+++ src/api/services/settlement/get-categories/index.ts
@@ -2,7 +2,7 @@ import axiosInstance from '../../../axiosInstance';
 import { Category } from './get.interface';
 
 export const GetCategories = async (): Promise<Category[]> => {
-  const Categories = await axiosInstance.get<Category[]>(`/settlement-project-categories`);
+  const { data } = await axiosInstance.get(`/settlement-project-categories`);
 
-  return Categories.data;
+  return data;
 };

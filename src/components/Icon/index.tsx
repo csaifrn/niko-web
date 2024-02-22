1d625ba14e5e2fe269196585d553eda64c5083e2
Pedro Henrique de Sousa Varela <78501819+Pedrohsv1@users.noreply.github.com>
2024-02-22 14:45:35 -0300

    feat: display image from user

--- src/components/Icon/index.tsx
+++ src/components/Icon/index.tsx
@@ -3,6 +3,7 @@ import { AssignedUser } from '../../api/services/batches/get-batche/get.interfac
 import { generateUUID } from '../../utils/generateUUID.util';
 import styled from 'styled-components';
 import theme from '../../global/theme';
+import { Photo } from '../../api/services/users/me/me.interface';
 
 interface Props {
   name: string;
@@ -10,9 +11,10 @@ interface Props {
   size?: number;
   color?: string;
   tooloff?: boolean;
+  photo?: Photo | undefined;
 }
 
-export const IconUser = ({ name, size = 24, fontSize = 12, color, tooloff = false }: Props) => {
+export const IconUser = ({ name, size = 24, fontSize = 12, color, tooloff = false, photo }: Props) => {
   function getInitials(name: string): string {
     const nameParts = name.split(' ');
     let initials = '';
@@ -30,24 +32,64 @@ export const IconUser = ({ name, size = 24, fontSize = 12, color, tooloff = fals
   const IconInitials = getInitials(name);
   const random = generateUUID();
 
-  return (
-    <BlackBlock
-      style={{
-        borderRadius: '100%',
-        width: `${size}px`,
-        height: `${size}px`,
-        padding: '0',
-        backgroundColor: `${color ? color : theme.colors['gray/700']}`,
-      }}
-      className={`my-tooltip-${random}`}
-    >
-      <p style={{ fontSize: `${fontSize}px`, color: theme.colors['white'] , fontFamily: 'Rubik'  }}>{IconInitials}</p>
-      {!tooloff && <Tooltip anchorSelect={`.my-tooltip-${random}`} children={<p>{name}</p>} place="top" />}
-    </BlackBlock>
-  );
+  if (photo) {
+    return (
+      <DivImgUser
+        style={{
+          width: `${size}px`,
+          height: `${size}px`,
+        }}
+        className={`my-tooltip-${random}-img`}
+      >
+        <ImgUser src={photo.url} />
+        {!tooloff && <Tooltip anchorSelect={`.my-tooltip-${random}-img`} children={<p>{name}</p>} place="top" />}
+      </DivImgUser>
+    );
+  } else {
+    return (
+      <BlackBlock
+        style={{
+          borderRadius: '100%',
+          width: `${size}px`,
+          height: `${size}px`,
+          padding: '0',
+          backgroundColor: `${color ? color : theme.colors['gray/700']}`,
+        }}
+        className={`my-tooltip-${random}`}
+      >
+        <p style={{ fontSize: `${fontSize}px`, color: theme.colors['white'], fontFamily: 'Rubik' }}>{IconInitials}</p>
+        {!tooloff && <Tooltip anchorSelect={`.my-tooltip-${random}`} children={<p>{name}</p>} place="top" />}
+      </BlackBlock>
+    );
+  }
 };
 
 const BlackBlock = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  :hover div {
+    visibility: visible;
+    opacity: 1;
+    display: block;
+  }
+`;
+
+const ImgUser = styled.img`
+  border-radius: 100%;
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+
+  :hover div {
+    visibility: visible;
+    opacity: 1;
+    display: block;
+  }
+`;
+
+const DivImgUser = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
